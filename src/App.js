6d5b1f2989ff2aecd81787ea3a86d8bfc71f943f
MrAbhishekRawat <abhishekr.rawat8@gmail.com>
2023-05-29 14:36:46 +0530

    movies task6.1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import  React, {useState} from 'react';
-
+import React, { useState } from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
- const [movies, setMovies]= useState([]);
+  const [movies, setMovies] = useState([]);
 
- function fetchMoviesHandler(){
-  fetch('https://swapi.dev/api/films/')
-  .then((res)=>{
-    return res.json()
-  })
-  .then((data)=>{
-    const transformedData= data.results.map(movieData=>{
-      return{
-        id: movieData.episode_id,
-        title: movieData.title ,
-        openingText: movieData.opening_crawl ,
-        releaseDate: movieData.release_date,
-      }
-    });
-    setMovies(transformedData)
-  })
- }
+  async function fetchMoviesHandler() {
+    try {
+      const response = await fetch('https://swapi.dev/api/films/');
+      const data = await response.json();
+      
+      const transformedData = data.results.map(movieData => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+
+      setMovies(transformedData);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
     <React.Fragment>
